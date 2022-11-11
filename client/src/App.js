d6a99d5cb907fa86da68302254dd2ea952f94fa9
connor rayburn <connorrayburn.work@gmail.com>
2022-11-11 13:44:24 -0700

    initialized react app and removed unnecessary files

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li className='left'><a href='/'>PIZZA<span className='redword'>PIZZA</span>PIZZA</a></li>
+            <li className='right'>Home</li>
+            <li className='right'>Login</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
